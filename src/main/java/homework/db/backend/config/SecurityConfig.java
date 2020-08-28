321f9bf9f526846a162bcc641bb18fe5619f79bd
gantinus <gantinus@gmail.com>
2020-08-28 23:39:14 +0800

    cors

--- src/main/java/homework/db/backend/config/SecurityConfig.java
+++ src/main/java/homework/db/backend/config/SecurityConfig.java
@@ -9,9 +9,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import javax.sql.DataSource;
 import java.util.Arrays;
@@ -68,6 +70,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .frameOptions()
             .sameOrigin();
         httpSecurity.cors().configurationSource(corsConfigurationSource());
+        httpSecurity.addFilterBefore(new CorsFilter(corsConfigurationSource()), CsrfFilter.class);
     }
 
     CorsConfigurationSource corsConfigurationSource() {
