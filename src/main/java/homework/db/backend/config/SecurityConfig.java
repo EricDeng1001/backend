34aecad1eb15d760cc9253e7615d10069aaef591
gantinus <gantinus@gmail.com>
2020-08-28 23:39:14 +0800

    cors
    (cherry picked from commit 321f9bf9f526846a162bcc641bb18fe5619f79bd)

--- src/main/java/homework/db/backend/config/SecurityConfig.java
+++ src/main/java/homework/db/backend/config/SecurityConfig.java
@@ -9,8 +9,15 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import javax.sql.DataSource;
+import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -68,7 +75,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         httpSecurity.headers()
             .frameOptions()
             .sameOrigin();
+        httpSecurity.cors().configurationSource(corsConfigurationSource());
+        httpSecurity.addFilterBefore(new CorsFilter(corsConfigurationSource()), CsrfFilter.class);
     }
 
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Collections.singletonList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "DELETE", "PUT"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 
 }
